183b9d6e3f3585810fe077423458e63cef5f7765
Navneet30125 <navneetlodhi73@gmail.com>
2025-08-10 10:49:57 +0530

    Revert "msg to message"
    This reverts commit 3d98016fe0ebb3e2b6fcdbe62240b7920ba573fc.

--- CarHeatException.java
+++ CarHeatException.java
@@ -1,8 +1,8 @@
 package car.related.custom_exception.program;
 
 public class CarHeatException extends Exception{
-public CarHeatException(String message) {
-	super(message);
+public CarHeatException(String msg) {
+	super(msg);
 }
 }
 
