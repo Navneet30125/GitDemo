4c523bf9e83da664721ee1e9a0d2516d18a7b8f0
Navneet30125 <navneetlodhi73@gmail.com>
2025-08-10 11:02:47 +0530

    java files updated

--- CarHeatException.java
+++ CarHeatException.java
@@ -2,6 +2,7 @@ package car.related.custom_exception.program;
 
 public class CarHeatException extends Exception{
 public CarHeatException(String msg) {
+int a = 30;
 	super(msg);
 }
 }
---
+++ Demo.java
@@ -0,0 +1,1 @@
+System.out.println("Tere world me tere naam ka yar");
